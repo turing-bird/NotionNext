532cd2e980cb44ae2981c1eff418f8fde09b77b1
turing-bird <52934646+turing-bird@users.noreply.github.com>
2024-07-02 16:52:31 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -16,7 +16,7 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '只讲实用',
-  HEO_HERO_TITLE_2: '\t\t不谈技术',
+  HEO_HERO_TITLE_2: '    不谈技术',
   HEO_HERO_TITLE_3: 'XiaoniaoAI',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'XiaoniaoAI 分享实用经验',
